a65d54be11df188727a7b3625ea9c58e88c3ee44
barsa <barsa@BAR>
2023-11-18 15:05:32 +0200

    Update

--- Assignment/DosProtection.Controllers/ShutdownController.cs
+++ Assignment/DosProtection.Controllers/ShutdownController.cs
@@ -2,6 +2,7 @@
 using Assignment.DosProtection.DM.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace Assignment.DosProtection.Controllers
@@ -18,12 +19,22 @@ namespace Assignment.DosProtection.Controllers
             _processEvent = processEvent;
         }
 
-        // Simulate receiving an HTTP request
+        // HTTP endpoint that mocks the key press signal.
         [HttpGet("shutdown/{key}")]
-        public IActionResult Shutdown(string key)
+        public HttpStatusCode Shutdown(string key)
         {
-            _processEvent.OnHttpRequestReceived(key);
-            return Ok();
+            _logger.LogDebug($"[ShutdownController:Shutdown] Key signal received: {key}");
+            try
+            {
+                _processEvent.OnHttpRequestReceived(key);
+                return HttpStatusCode.OK;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"[ShutdownController:Shutdown] Error: {ex.Message}");
+                return HttpStatusCode.InternalServerError;
+            }
+
         }
     }
 }
