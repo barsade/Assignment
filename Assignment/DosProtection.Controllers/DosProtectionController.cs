f2043ff899d70dadb3c9dd6b4ad85211f0e00e1d
barsa <barsa@BAR>
2023-11-18 11:27:24 +0200

    Adjusting api

--- Assignment/DosProtection.Controllers/DosProtectionController.cs
+++ Assignment/DosProtection.Controllers/DosProtectionController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Http.Features;
 
 namespace Assignment.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class DosProtectionController : ControllerBase
     {
