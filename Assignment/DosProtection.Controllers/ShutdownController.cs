e9d42e00f718d8302ef24b0760df99e0d6489a31
barsa <barsa@BAR>
2023-11-19 07:31:39 +0200

    a

--- Assignment/DosProtection.Controllers/ShutdownController.cs
+++ Assignment/DosProtection.Controllers/ShutdownController.cs
@@ -17,7 +17,12 @@ namespace Assignment.DosProtection.Controllers
             _logger = logger;
         }
 
-        // HTTP endpoint that mocks the key press signal.
+        // This API endpoint serves as a mock to simulate a key press event pressed by the server,
+        // since Web API is stateless and cannot get user input (only in the form of HTTP requests).
+        // Besides the trigger of the key press (input event vs HTTP request), the logic implemented is the same
+        // once the event is triggered.
+        // The listener for the key press event is implemented in the Program.cs file in a separate task running
+        // throughout the application's lifetime in the background.
         [HttpGet("shutdown/{key}")]
         public HttpStatusCode Shutdown(string key)
         {
